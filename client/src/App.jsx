70252afa48f2341fc6ba8420eed8523145d6d236
Ranjith <ranjithsm459@gmail.com.com>
2023-10-10 12:54:22 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+<h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-500 text-3xl  font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
