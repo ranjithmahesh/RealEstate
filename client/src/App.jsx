95fb2318969b32e73e872cb730018023b08223bb
Ranjith <ranjithsm459@gmail.com.com>
2023-10-10 14:27:07 +0530

    create Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import SingIn from "./pages/SingIn";
 import SingUp from "./pages/SingUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./component/Header";
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
 
