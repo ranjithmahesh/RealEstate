4c9d06ab8e988e9712bf31fea4cc917324263d71
Ranjith <ranjithsm459@gmail.com.com>
2023-10-15 00:07:31 +0530

    complete create listing page functionality

--- API/models/listing.modle.js
+++ API/models/listing.modle.js
@@ -6,7 +6,7 @@ const listingSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    discription: {
+    description: {
       type: String,
       required: true,
     },
